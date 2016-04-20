94a679605ab0b6a04c5e2d23615bcfd91f291686
Hasan Hafiz Pasha <mac_pasha@ovi.com>
2016-04-21 02:45:29 +0600

    SIgn Up Complete

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Controllers;
+
+class HomeController extends Controller{
+    public function index($request, $response){
+        return $this->view->render($response, 'home.twig');
+    }
+}
---
+++ bootstrap/app.php
@@ -0,0 +1,74 @@
+<?php
+
+use Respect\Validation\Validator as v;
+
+session_start();
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+        'db' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'photographer',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]
+    ]
+]);
+
+$container = $app->getContainer();
+
+$capsule =  new Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+$container['db']    =   function($container) use($capsule){
+    return $capsule;
+};
+
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
+        'cache' => false,
+    ]);
+
+    $view->addExtension(new Slim\Views\TwigExtension(
+        $container->router,
+        $container->request->getUri()
+    ));
+    return $view;
+};
+
+$container['validator']  =   function($container){
+    return new App\Validation\Validator;
+};
+
+$container['HomeController'] = function ($container) {
+    return new App\Controllers\HomeController($container);
+};
+
+$container['AuthController'] = function ($container) {
+    return new App\Controllers\Auth\AuthController($container);
+};
+
+$container['csrf'] = function ($container) {
+    return new \Slim\Csrf\Guard;
+};
+
+$app->add(new  \App\Middleware\ValidationErrorsMiddleware($container));
+$app->add(new  \App\Middleware\OldInputMiddleware($container));
+$app->add(new  \App\Middleware\CsrfViewMiddleware($container));
+
+$app->add($container->csrf);
+
+v::with('App\\Validation\\Rules\\');
+
+require __DIR__ . '/../app/routes.php';
